86515bace54204a3f25359ca69523fbd054a8083
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-11 15:19:15 +0200

    Hello World App With .NET5 and .NET6 Style

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿// using System;
+
+// namespace HelloWorld{
+//   class Program{
+//     public static void Main(string[] args) {
+//       Console.WriteLine("Hello Please Enter Your Name");
+
+//       string? name = Console.ReadLine();
+
+//       Console.WriteLine("Hello " + name);
+//     }
+//   }
+// }
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello Please Enter Your Name");
+
+string? name = Console.ReadLine();
+
+Console.WriteLine("Hello " + name);
+
+
+
