1b870f941eadee36398d534780685876f858e1d2
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-11 21:23:53 +0200

    Working with C# Exceptions

---
+++ 7-Exceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int x = 1000000;
+int y = 0;
+
+
+
+try
+{
+  if (x > 1000)
+  {
+    throw new ArgumentOutOfRangeException("X Has to be Less Than 1000");
+  }
+  int result = x / y;
+  Console.WriteLine("The Result = ", result);
+}
+catch (DivideByZeroException e)
+{
+
+  Console.WriteLine("Number Cannot be Divided by Zero");
+  Console.WriteLine(e.Message);
+}
+catch (ArgumentOutOfRangeException e)
+{
+  Console.WriteLine("Number Cannot be Divided by Zero");
+  Console.WriteLine(e.Message);
+}
