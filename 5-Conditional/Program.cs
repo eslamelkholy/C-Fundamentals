0e236feaedd277e8e78e0d7086e351e65dfe3c1c
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-11 19:00:34 +0200

    Working WIth Switch Statement

--- 5-Conditional/Program.cs
+++ 5-Conditional/Program.cs
@@ -1,15 +1,33 @@
-﻿const int ALLOWED_AGE = 18;
+﻿// const int ALLOWED_AGE = 18;
 
-Console.WriteLine("Hello Enter your Age");
-string? userAge = Console.ReadLine();
+// Console.WriteLine("Hello Enter your Age");
+// string? userAge = Console.ReadLine();
 
-int age = -1;
+// int age = -1;
 
-if (int.TryParse(userAge, out age) && age > ALLOWED_AGE)
+// if (int.TryParse(userAge, out age) && age > ALLOWED_AGE)
+// {
+//   Console.WriteLine("Welcomeeeee!");
+// }
+// else
+// {
+//   Console.WriteLine("Sorry Greater than 18 only allowed here");
+// }
+
+
+Console.WriteLine("Hello Please Enter your Role....");
+string? userRole = Console.ReadLine();
+
+switch (userRole)
 {
-  Console.WriteLine("Welcomeeeee!");
-}
-else
-{
-  Console.WriteLine("Sorry Greater than 18 only allowed here");
+  case "admin":
+    Console.WriteLine("OH Hello Admin");
+    break;
+  case "user":
+    Console.WriteLine("OH Hello User");
+    break;
+
+  default:
+    Console.WriteLine("PLEASE Enter a Valid Role");
+    break;
 }
