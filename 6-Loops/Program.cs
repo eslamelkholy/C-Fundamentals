9de52c3058d8698ab790ce4880ca2345269f969c
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-11 21:11:43 +0200

    WOrking With Loops For and While

---
+++ 6-Loops/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] nums = { 2, 4, 6, 7, 8, 9, 12, 13, 14, 15, 16 };
+
+string str = "The Quick Brown Fox Jump Over the Lazy Dog";
+
+foreach (var item in nums)
+{
+  Console.WriteLine("Num = " + item);
+}
+
+foreach (var c in str)
+{
+  Console.WriteLine("Char = " + c);
+}
+
+
+Console.WriteLine("This is While Loop");
+
+string inputName = "";
+
+while (inputName != "Eslam")
+{
+  Console.WriteLine("Say my Name ");
+  inputName = Console.ReadLine() ?? "";
+}
+
+
