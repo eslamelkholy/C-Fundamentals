9469a95eecc4fccb0016be17fa3e9164aee87969
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-11 18:56:00 +0200

    Working with Some Console Conditions

---
+++ 5-Conditional/Program.cs
@@ -0,0 +1,15 @@
+﻿const int ALLOWED_AGE = 18;
+
+Console.WriteLine("Hello Enter your Age");
+string? userAge = Console.ReadLine();
+
+int age = -1;
+
+if (int.TryParse(userAge, out age) && age > ALLOWED_AGE)
+{
+  Console.WriteLine("Welcomeeeee!");
+}
+else
+{
+  Console.WriteLine("Sorry Greater than 18 only allowed here");
+}
