3c33bc603a782e63a0b43016f5e820ef4ce2d488
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-12 17:14:58 +0200

    C# isPalindrom Function

---
+++ 9-Functions/5-Palindrom/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, This Program Determine if String is Palindrom or Not");
+
+
+bool isPalindrom(string str)
+{
+  str = str.ToLower();
+  for (int i = 0; i < str.Length; i++)
+  {
+    if (Char.IsPunctuation(str[i])) continue;
+
+    if (str[i] != str[str.Length - i - 1])
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+while (true)
+{
+  Console.WriteLine("Please Enter The String");
+  string? str = Console.ReadLine();
+  if (str is null) continue;
+
+  if (isPalindrom(str))
+  {
+    Console.WriteLine($"{str} Is Palindrom True and Length = {str.Length}");
+  }
+  else
+  {
+    Console.WriteLine($"{str} is Not Palindrom");
+  }
+
+}
