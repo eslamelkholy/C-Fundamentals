01499c915b3f567973b69291eba9b3d3f69e6034
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-12 16:27:53 +0200

    Functions Default Paramters

---
+++ 9-Functions/2-DefaultParams/Program.cs
@@ -0,0 +1,11 @@
+﻿void prefixAppend(string str, string prefix = "Prefix")
+{
+  Console.WriteLine($"{prefix} {str}");
+}
+
+prefixAppend("String");
+
+
+// TODO: Call Function with Parameters at any Order
+
+prefixAppend(prefix: " Pre", str: "Str");
