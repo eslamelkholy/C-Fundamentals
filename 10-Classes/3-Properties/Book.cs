9d028fb4e95019f546e786bb82877f54f5219625
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-12 18:10:10 +0200

    Working with Properties Setter and getter

---
+++ 10-Classes/3-Properties/Book.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Properties
+{
+
+  public class Book
+  {
+    private string _name;
+    private string _author;
+    private int _pageCount;
+
+    public Book(
+      string name,
+      string author,
+      int pageCount
+    )
+    {
+      Name = name;
+      Author = author;
+      PageCount = pageCount;
+    }
+
+    public int PageCount { get => _pageCount; set => _pageCount = value; }
+    public string Name { get => _name; set => _name = value; }
+    public string Author { get => _author; set => _author = value; }
+
+    public string getDescription()
+    {
+      return $"The Book Name is {Name} and Author is {Author} And Pages = {PageCount}";
+    }
+
+    // TODO: Properties can be Auto Generates
+    public string ISBN
+    {
+      get;
+      set;
+    }
+
+    public decimal Price
+    {
+      get;
+      set;
+    }
+  }
+}
---
+++ 10-Classes/3-Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Properties
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      // Create new object instances using the "new" operator
+      Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+      Book b2 = new Book("The Grapes of Wrath", "John Steinbeck", 464);
+
+      b1.Name = "Eslam";
+
+      b1.PageCount = 10000;
+      b1.Price = 10000;
+
+      Console.WriteLine(b1.Price);
+      Console.WriteLine(b1.PageCount);
+    }
+  }
+}
