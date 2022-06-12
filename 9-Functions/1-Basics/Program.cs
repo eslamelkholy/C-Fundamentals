ce0d3c14aebd4486e5102f775c606f7de861ce51
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-12 16:20:29 +0200

    Working with C# Functions

---
+++ 9-Functions/1-Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿float MilesToKm(float miles)
+{
+  float result = miles * 1.6f;
+
+  return result;
+}
+
+
+void printMilesToKm(float miles)
+{
+  Console.WriteLine(MilesToKm(miles));
+}
+
+
+printMilesToKm(10000);
