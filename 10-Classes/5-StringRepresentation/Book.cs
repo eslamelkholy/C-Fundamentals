3240ba6160f30689d8e7d0bbf62c77932c027fab
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-13 15:13:38 +0200

    Override and Overload to String Function in C#

---
+++ 10-Classes/5-StringRepresentation/Book.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace StringRep
+{
+  class Book
+  {
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public int PageCount { get; }
+
+    public Book(string name, string author, int pages)
+    {
+      Name = name;
+      Author = author;
+      PageCount = pages;
+    }
+
+    // TODO: The ToString method generates a string represenation of the object
+
+    public override string ToString()
+    {
+      return $"Book Name {Name} And Author is {Author}";
+    }
+
+
+    // TODO: ToString can be overloaded to give different format versions
+    // Notice that this version is NOT an override function
+
+    public string ToString(char format)
+    {
+      if (format == 'B')
+      {
+        return $"Book Name = {Name}";
+      }
+
+      if (format == 'F')
+      {
+        return $"Book Name {Name} And Author is {Author} and Page Count = {PageCount}";
+
+      }
+
+      return ToString();
+    }
+
+  }
+}
