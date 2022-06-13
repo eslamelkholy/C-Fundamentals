a55aa87952baa0796ce6585f41fc7704c6838516
eslamelkholy <eslamelkholy5325762@gmail.com>
2022-06-13 16:17:42 +0200

    Debug C# Code With Paramters

---
+++ 11-Debug/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Debug
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      if (args.Length > 0)
+      {
+        Console.WriteLine($" This One Will Throw Error {args[0]}");
+      }
+      else
+      {
+        Console.WriteLine($" This One Will Not Throw Error");
+
+      }
+    }
+  }
+}
